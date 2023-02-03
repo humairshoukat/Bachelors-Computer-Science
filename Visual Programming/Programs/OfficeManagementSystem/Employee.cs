dec72029489e1701d4757439833898099e64cc5a
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2023-02-03 20:10:56 +0500

    Added Files

---
+++ Visual Programming/Programs/OfficeManagementSystem/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeManagementSystem
+{
+    internal class Employee
+    {
+        int ID;
+        String Name;
+        String Cell;
+        String Address;
+
+        public Employee(int id, string name, string cell, string address)
+        {
+            ID=id;
+            Name=name;
+            Cell=cell;
+            Address=address;
+        }
+    }
+}
