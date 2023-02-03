0e064cf567df538cec2f76a51156735d6ea2a7a2
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2023-02-03 18:14:35 +0500

    Added Files

---
+++ Digital Logic Design/DLD Lab Programs/Half Adder and Full Adder/untitled0Tim.v
@@ -0,0 +1,22 @@
+// Auto-generated test bench created by VeriLogger Pro at Sun May 02 20:07:44 2021
+
+// Timing model is min/max timing. 
+
+`timescale 1ns / 1ps
+
+module syncad_top;
+
+
+wire [1:0] tb_status;
+reg [1:0] tb_status_driver;
+assign tb_status = tb_status_driver;
+
+initial tb_status_driver = 1'b1; //diagram always running in auto-generated file
+
+test test();
+
+  initial
+    begin
+    end
+endmodule
+
---
+++ Digital Logic Design/DLD Lab Programs/Mux & Demux/2302HumairShoukat.v
@@ -0,0 +1,29 @@
+module humair1x4DEMUX(D3,D2,D1,D0,A,B,I);
+input A,B,I;
+output D3,D2,D1,D0;
+wire Anot,Bnot;
+not (Anot, A);
+not (Bnot, B);
+and (D0, Anot, Bnot, I);
+and (D1, Anot, B, I);
+and (D2, A, Bnot, I);
+and (D3, A, B, I);
+endmodule
+
+module test;
+reg A,B,I;
+wire D3,D2,D1,D0;
+humair1x4DEMUX x(D3,D2,D1,D0,A,B,I);
+initial
+begin
+A=1'b 0;B=1'b 0;I=1'b 0;#10
+A=1'b 0;B=1'b 0;I=1'b 1;#10
+A=1'b 0;B=1'b 1;I=1'b 0;#10
+A=1'b 0;B=1'b 1;I=1'b 1;#10
+A=1'b 1;B=1'b 0;I=1'b 0;#10
+A=1'b 1;B=1'b 0;I=1'b 1;#10
+A=1'b 1;B=1'b 1;I=1'b 0;#10
+A=1'b 1;B=1'b 1;I=1'b 1;#10
+A=1'b 0;B=1'b 0;I=1'b 0;
+end
+endmodule
