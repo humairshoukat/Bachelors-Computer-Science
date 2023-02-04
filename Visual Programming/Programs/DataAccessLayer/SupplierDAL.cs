35d4fd78397cf0db2f67832b311816605612a2ca
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2023-02-04 15:08:28 +0500

    Added Files

---
+++ Visual Programming/Programs/DataAccessLayer/SupplierDAL.cs
@@ -0,0 +1,31 @@
+﻿using AppProps;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class SupplierDAL
+    {
+        DbCon obj = new DbCon();
+        public bool AddSupplierDAL(Supplier s)
+        {
+            return obj.UDI("INSERT INTO tblSupplier (supId, supName, supCell, supAddress) VALUES('"+s.Id+"', '"+s.Name+"', '"+s.Cell+"', '"+s.Address+"')");
+        }
+        public bool UpdateSupplierDAL(Supplier s)
+        {
+            return obj.UDI("update tblSupplier set supName='"+s.Name+"' , supCell='"+s.Cell+"' , supAddress='"+s.Address+"' where supId='"+s.Id+"'");
+        }
+        public bool DeleteSupplierDAL(Supplier s)
+        {
+            return obj.UDI("delete tblSupplier where supId = '"+s.Id+"'");
+        }
+        public DataTable SearchSupplierDAL(Supplier s)
+        {
+            return obj.Search("Select * from tblSupplier where supId='"+s.Id+"'");
+        }
+    }
+}
---
+++ Visual Programming/Programs/DataService/App_Code/IService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	string GetData(int value);
+
+	[OperationContract]
+	CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    // TODO: Add your service operations here
+
+    [OperationContract]
+    double ConvertToRupees(double amt);
+
+    DataTable/DataSet ConveShowFlightData(double amt);
+}
+
+// Use a data contract as illustrated in the sample below to add composite types to service operations.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
