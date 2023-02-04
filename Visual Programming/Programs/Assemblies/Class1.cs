93318a3febb6378aa214f707afc8bff6b405e8e9
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2023-02-04 15:12:05 +0500

    Added Files

---
+++ Visual Programming/Programs/Assemblies/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace Assemblies
+{
+    public class Class1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Fun(List<int> arr)
+        {
+            int min=0;
+            int max=0;
+            int avg=0;
+            for(int i = 0; i < arr.Count; i++)
+            {
+                avg=avg+arr[i];
+                for(int j=0; j<arr.Count; j++)
+                {
+                    if(arr[i] > arr[j])
+                    {
+                        max = arr[i];   
+                    }
+                    if(arr[i] < arr[j])
+                    {
+                        min = arr[i];
+                    }
+                }
+            }
+            avg=avg/arr.Count;
+            return min+max+avg;
+        }
+    }
+}
